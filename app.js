1e550c875c86995d46b0d32ccbcf1893ef548655
Manthan Laad <laad.manthan@gmail.com>
2020-09-24 14:09:03 +0530

    Fixed Burger Menu Scrolling

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 const nav = document.querySelector(".nav-links");
 const burger = document.querySelector(".burger");
 const links = nav.querySelectorAll("a");
+const body = document.querySelector("body");
 
 burger.addEventListener("click", () => {
   nav.classList.toggle("nav-open");
   burger.classList.toggle("toggle");
+  body.classList.toggle("no-scroll");
 });
 
 links.forEach((link) => {
   link.addEventListener("click", () => {
     nav.classList.toggle("nav-open");
     burger.classList.toggle("toggle");
+    body.classList.toggle("no-scroll");
   });
 });
